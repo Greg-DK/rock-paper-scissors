04ee20b1f316f559f7b86246f83ed5fbbd7623e9
Greg-DK <67904364+Gdekwant@users.noreply.github.com>
2022-12-28 19:24:29 +1000

    edited RPS game to use UI

--- main.js
+++ main.js
@@ -2,86 +2,101 @@
 
 let choices = ["rock", "paper", "scissors"]
 
+let roundsPlayed = 0;
+let pwins = document.getElementById("pwins");
+let cwins = document.getElementById("cwins");
+let roundResult;
+
+const rock = document.getElementById('rock');
+const scissors = document.getElementById('scissors');
+const paper = document.getElementById('paper');
+const result = document.getElementById('result');
+
+
+let computerWins = 0;
+let playerWins = 0;
+pwins.innerText = playerWins;
+cwins.innerText = computerWins;
+
+rock.addEventListener('click', playRound);
+paper.addEventListener('click', playRound);
+scissors.addEventListener('click', playRound);
+console.log(roundResult)
+    
+
+
 function getComputerChoice() {
     // use a random function to iterate 0-3 and index choices
     let randomNumber = Math.floor((Math.random()) * choices.length);
     let computerChoice = choices[randomNumber];
-    return computerChoice
+    return computerChoice;
 }
 
-function getPlayerChoice() {
-    // provide an input box to choice rock, paper or scissors
-    // make the input case insensitive (run a string.lower on the result)
-    let playerChoice = prompt('Enter your choice:', "rock");
-    playerChoice = playerChoice.toLowerCase();
-    return playerChoice
-}
-
-function playRound() {
+function playRound(button) {
     //compare computerChoice to playerChoice using if statements
     let computerChoice = getComputerChoice();
-    let playerChoice = getPlayerChoice();
-    let roundResult
+    let playerChoice = button.target.id;
+    let roundResult;
+    let choice = document.getElementById('choice');
+    let output = document.getElementById('output');
+    choice.innerText = "You chose " + playerChoice;
+    result.innerText = ''
     if (computerChoice === playerChoice) {
-        roundResult = "draw"
-        console.log("This round is a draw.")
+        roundResult = "draw";
+        output.innerText = ("This round is a draw.");
     }
     if (computerChoice === choices[0] && playerChoice === choices[1]) {
-        roundResult = "win"
-        console.log("You win this round! " + playerChoice + " beats " + computerChoice + ".")
+        roundResult = "win";
+        output.innerText = ("You win this round! " + playerChoice + " beats " + computerChoice + ".");
     }
     if (computerChoice === choices[0] && playerChoice === choices[2]) {
         roundResult = "lose"
-        console.log("You lose this round. " + computerChoice + " beats " + playerChoice+ ".")
+        output.innerText = ("You lose this round. " + computerChoice + " beats " + playerChoice+ ".")
     }
     if (computerChoice === choices[1] && playerChoice === choices[2]) {
         roundResult = "win"
-        console.log("You win this round! " + playerChoice + " beats " + computerChoice + ".")
+        output.innerText = ("You win this round! " + playerChoice + " beats " + computerChoice + ".")
     }
     if (computerChoice === choices[1] && playerChoice === choices[0]) {
         roundResult = "lose"
-        console.log("You lose this round. " + computerChoice + " beats " + playerChoice+ ".")
+        output.innerText = ("You lose this round. " + computerChoice + " beats " + playerChoice+ ".")
     }
     if (computerChoice === choices[2] && playerChoice === choices[0]) {
         roundResult = "win"
-        console.log("You win this round! " + playerChoice + " beats " + computerChoice + ".")
+        output.innerText = ("You win this round! " + playerChoice + " beats " + computerChoice + ".")
     }
     if (computerChoice === choices[2] && playerChoice === choices[1]) {
         roundResult = "lose"
-        console.log("You lose this round. " + computerChoice + " beats " + playerChoice+ ".")
+        output.innerText = ("You lose this round. " + computerChoice + " beats " + playerChoice+ ".")
+    }
+
+    console.log(roundResult)
+    if (roundResult === "lose") {
+        computerWins = ++computerWins;
+        cwins.innerText = computerWins;
+    }
+
+    if (roundResult === "win") {
+        playerWins = ++playerWins;
+        pwins.innerText = playerWins;
+    }
+
+    if (computerWins === 5) {
+        result.innerText = ("Final Result: You lose. " + computerWins + " to " +playerWins)
+        resetGame();
+    }
+    
+    if (playerWins === 5) {
+        result.innerText = ("Final Result: You Won! " + playerWins +" to " + computerWins)
+        resetGame();
     }
     return roundResult
 }
 
-
-function game() {
-    //play a best of 5 rounds and console.log the result
-    let computerWins = 0
-    let playerWins = 0
-    let roundsPlayed = 0
-    for (;roundsPlayed<5;) {
-      let roundResult = playRound() 
-      if (roundResult === "lose") {
-        computerWins += 1; 
-        roundsPlayed += 1;
-      }
-      if (roundResult === "win") {
-        playerWins += 1;
-        roundsPlayed += 1;
-      }
-      
-      console.log("Computer: " + computerWins + " You: " + playerWins)
-    }
-    if (computerWins > playerWins) {
-      console.log("Final Result: You lose. " + computerWins + " to " +playerWins)
-    }
-    if (computerWins === playerWins) {
-      console.log("Final Result: It's a draw!")
-    }
-    if (computerWins < playerWins) {
-      console.log("Final Result: You Won! " + playerWins +" to " + computerWins)
-    }
-  }
-
-  game()
-    
+function resetGame() {
+    playerWins = 0;
+    computerWins = 0;
+    output.innerText = "Reset for the next game.";
+    pwins.innerText = playerWins;
+    cwins.innerText = computerWins; 
+}
